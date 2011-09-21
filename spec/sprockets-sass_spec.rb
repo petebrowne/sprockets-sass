3386fb0d4d813b6da1e475c21bff07d855213512
Pete Browne <me@petebrowne.com>
2011-09-21 09:46:54 -0500

    Renaming variables used in specs to be more clear

--- spec/sprockets-sass_spec.rb
+++ spec/sprockets-sass_spec.rb
@@ -13,69 +13,69 @@ describe Sprockets::Sass do
   end
   
   it "processes scss files normally" do
-    @assets.file "application.css.scss", "//= require posts.css.scss"
-    @assets.file "posts.css.scss", ".post { color: blue; }"
-    asset = @env["application.css.scss"]
-    asset.to_s.should == ".post {\n  color: blue; }\n"
+    @assets.file "main.css.scss", "//= require dep"
+    @assets.file "dep.css.scss", "body { color: blue; }"
+    asset = @env["main.css.scss"]
+    asset.to_s.should == "body {\n  color: blue; }\n"
   end
   
   it "processes sass files normally" do
-    @assets.file "application.css.sass", "//= require posts.css.sass"
-    @assets.file "posts.css.sass", ".post\n  color: blue"
-    asset = @env["application.css.sass"]
-    asset.to_s.should == ".post {\n  color: blue; }\n"
+    @assets.file "main.css.sass", "//= require dep"
+    @assets.file "dep.css.sass", "body\n  color: blue"
+    asset = @env["main.css.sass"]
+    asset.to_s.should == "body {\n  color: blue; }\n"
   end
   
   it "imports standard files" do
-    @assets.file "application.css.scss", '@import "posts"'
-    @assets.file "posts.css.scss", ".post { color: blue; }"
-    asset = @env["application.css.scss"]
-    asset.to_s.should == ".post {\n  color: blue; }\n"
+    @assets.file "main.css.scss", '@import "dep"'
+    @assets.file "dep.css.scss", "body { color: blue; }"
+    asset = @env["main.css.scss"]
+    asset.to_s.should == "body {\n  color: blue; }\n"
   end
   
   it "imports partial style files" do
-    @assets.file "application.css.scss", '@import "posts"'
-    @assets.file "_posts.css.scss", ".post { color: blue; }"
-    asset = @env["application.css.scss"]
-    asset.to_s.should == ".post {\n  color: blue; }\n"
+    @assets.file "main.css.scss", '@import "dep"'
+    @assets.file "_dep.css.scss", "body { color: blue; }"
+    asset = @env["main.css.scss"]
+    asset.to_s.should == "body {\n  color: blue; }\n"
   end
   
   it "imports relative files" do
-    @assets.file "application/main.css.scss", '@import "./posts"'
-    @assets.file "application/posts.css.scss", ".post { color: blue; }"
-    asset = @env["application/main.css.scss"]
-    asset.to_s.should == ".post {\n  color: blue; }\n"
+    @assets.file "folder/main.css.scss", '@import "./dep"'
+    @assets.file "folder/dep.css.scss", "body { color: blue; }"
+    asset = @env["folder/main.css.scss"]
+    asset.to_s.should == "body {\n  color: blue; }\n"
   end
   
   it "imports files from the assets load path" do
     vendor = @root.directory "vendor"
     @env.append_path vendor.to_s
     
-    @assets.file "application.css.scss", '@import "posts"'
-    vendor.file "posts.css.scss", ".post { color: blue; }"
-    asset = @env["application.css.scss"]
-    asset.to_s.should == ".post {\n  color: blue; }\n"
+    @assets.file "main.css.scss", '@import "dep"'
+    vendor.file "dep.css.scss", "body { color: blue; }"
+    asset = @env["main.css.scss"]
+    asset.to_s.should == "body {\n  color: blue; }\n"
   end
   
   it "imports files from the Sass load path" do
     vendor = @root.directory "vendor"
     Sass::Engine::DEFAULT_OPTIONS[:load_paths] << vendor.to_s
     
-    @assets.file "application.css.scss", '@import "posts"'
-    vendor.file "posts.scss", ".post { color: blue; }"
-    asset = @env["application.css.scss"]
-    asset.to_s.should == ".post {\n  color: blue; }\n"
+    @assets.file "main.css.scss", '@import "dep"'
+    vendor.file "dep.scss", "body { color: blue; }"
+    asset = @env["main.css.scss"]
+    asset.to_s.should == "body {\n  color: blue; }\n"
   end
 
   it "adds dependency when imported" do
-    @assets.file "application.css.scss", '@import "posts"'
-    dep = @assets.file "posts.css.scss", ".post { color: blue; }"
+    @assets.file "main.css.scss", '@import "dep"'
+    dep = @assets.file "dep.css.scss", "body { color: blue; }"
     
-    asset = @env["application.css.scss"]
+    asset = @env["main.css.scss"]
     asset.should be_fresh
     
     mtime = Time.now + 1
-    dep.open("w") { |f| f.write ".post { color: red; }" }
+    dep.open("w") { |f| f.write "body { color: red; }" }
     dep.utime mtime, mtime
     
     asset.should be_stale
